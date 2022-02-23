c5e6d0ec2bf064820626bbc416c0c9cef237060b
Erick Velasco <erickvelasco11@gmail.com>
2022-02-23 16:36:02 -0500

    Download video flux complete

--- Downloader/Form1.Designer.cs
+++ Downloader/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.prgDownload = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(713, 100);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(12, 27);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(776, 23);
+            this.txtLink.TabIndex = 1;
+            // 
+            // prgDownload
+            // 
+            this.prgDownload.Location = new System.Drawing.Point(12, 71);
+            this.prgDownload.Name = "prgDownload";
+            this.prgDownload.Size = new System.Drawing.Size(776, 23);
+            this.prgDownload.TabIndex = 2;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 53);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(61, 15);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "Progress...";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(12, 9);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(143, 15);
+            this.lblUrl.TabIndex = 4;
+            this.lblUrl.Text = "Paste YouTube URL here...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 133);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.prgDownload);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnDownload;
+        private TextBox txtLink;
+        private ProgressBar prgDownload;
+        private Label lblProgress;
+        private Label lblUrl;
     }
 }
--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -1,10 +1,156 @@
+using DotNetTools.SharpGrabber;
+using DotNetTools.SharpGrabber.Converter;
+using DotNetTools.SharpGrabber.Grabbed;
+
+using System;
+
 namespace Downloader
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient Client = new();
+        private static readonly HashSet<string> TempFiles = new HashSet<string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            var grabber = GrabberBuilder.New()
+                .UseDefaultServices()
+                .AddYouTube()
+                .Build();
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var result = await grabber.GrabAsync(new Uri(txtLink.Text));
+                    var info = result.Resource<GrabbedInfo>();
+                    var images = result.Resources<GrabbedImage>().ToArray();
+                    var mediaFiles = result.Resources<GrabbedMedia>().ToArray();
+                    var sortedMediaFiles = mediaFiles.OrderByResolutionDescending().ThenByBitRateDescending().ToArray();
+                    var bestVideo = mediaFiles.GetHighestQualityVideo();
+                    var bestAudio = mediaFiles.GetHighestQualityAudio();
+
+
+                    var audioStream = ChooseMonoMedia(result, MediaChannels.Audio);
+                    var videoStream = ChooseMonoMedia(result, MediaChannels.Video);
+                    var audioPath = await DownloadMedia(audioStream, result);
+                    var videoPath = await DownloadMedia(videoStream, result);
+                    GenerateOutputFile(audioPath, videoPath, videoStream);
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        private void GenerateOutputFile(string audioPath, string videoPath, GrabbedMedia videoStream)
+        {
+            FFmpeg.AutoGen.ffmpeg.RootPath = "C:\\Users\\erick\\source\\repos\\Downloader\\Test\\ffmpeg";
+            var outputPath = "D:\\Documents\\video";
+            if (string.IsNullOrWhiteSpace(outputPath))
+                throw new Exception("No output path is specified.");
+            var merger = new MediaMerger(outputPath);
+            merger.AddStreamSource(audioPath, MediaStreamType.Audio);
+            merger.AddStreamSource(videoPath, MediaStreamType.Video);
+            merger.OutputMimeType = videoStream.Format.Mime;
+            merger.OutputShortName = videoStream.Format.Extension;
+            merger.Build();
+        }
+
+        private async Task<string> DownloadMedia(GrabbedMedia media, IGrabResult grabResult)
+        {
+            lblProgress.Invoke(new Action(() => {
+                lblProgress.Text = "Esperando...";
+                prgDownload.Value = 0;
+            }));
+
+            using var response = await Client.GetAsync(media.ResourceUri, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var total = response.Content.Headers.ContentLength ?? -1L;
+            using var downloadStream = await response.Content.ReadAsStreamAsync();
+            //using var resourceStream = await grabResult.WrapStreamAsync(downloadStream);
+
+            var downloadRate = 1048576;
+            var path = Path.GetTempFileName();
+            TempFiles.Add(path);
+            var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, downloadRate, true);
+
+            var read = 0;
+            var totalRead = 0L;
+            var buffer = new byte[downloadRate];
+            CancellationToken token = new();
+
+            lblProgress.Invoke(new Action(() => {
+                lblProgress.Text = "Total: " + total;
+            }));
+
+            do
+            {
+                token.ThrowIfCancellationRequested();
+
+                read = await downloadStream.ReadAsync(buffer, token);
+                var data = new byte[read];
+                buffer.ToList().CopyTo(0, data, 0, read);
+
+                await fileStream.WriteAsync(buffer.AsMemory(0, read));
+
+                // Update the percentage of file downloaded
+                totalRead += read;
+
+                lblProgress.Invoke(new Action(() =>
+                {
+                    var downloadPercentage = totalRead * 1d / (total * 1d) * 100;
+                    lblProgress.Text = "Downloading " + media.Channels + " " + grabResult.Title + ": " + downloadPercentage.ToString("0.##") + "%" + " " + (totalRead / 1000000.0).ToString("0.##") + "MB";
+                    prgDownload.Value = Convert.ToInt32(downloadPercentage);
+                }));
+            }
+            while (read > 0);
+
+
+            //using var fileStream = new FileStream(path, FileMode.Create);
+            
+            return path;
+        }
+
+        private GrabbedMedia ChooseMonoMedia(GrabResult result, MediaChannels channel)
+        {
+            var resources = result.Resources<GrabbedMedia>()
+               .Where(m => m.Channels == channel)
+               .ToList();
+
+            if (resources.Count == 0)
+                return null;
+
+            return channel == MediaChannels.Audio ? resources.GetHighestQualityAudio() : resources.GetHighestQualityVideo();
+
+            //for (var i = 0; i < resources.Count; i++)
+            //{
+            //    var resource = resources[i];
+            //    Console.WriteLine($"{i}. {resource.Title ?? resource.FormatTitle ?? resource.Resolution}");
+            //}
+
+            //while (true)
+            //{
+            //    Console.Write($"Choose the {channel} file: ");
+            //    var choiceStr = Console.ReadLine();
+            //    if (!int.TryParse(choiceStr, out var choice))
+            //    {
+            //        Console.WriteLine("Number expected.");
+            //        continue;
+            //    }
+
+            //    if (choice < 0 || choice >= resources.Count)
+            //    {
+            //        Console.WriteLine("Invalid number.");
+            //        continue;
+            //    }
+
+            //    return resources[choice];
+            //}
         }
     }
 }
