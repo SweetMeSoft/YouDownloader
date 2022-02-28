50d9836644b50b13c33dc88dd83c6f42030419b5
Erick Velasco <erickvelasco11@gmail.com>
2022-02-27 22:03:02 -0500

    Added VideoLibrary

--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 using System.Diagnostics;
 
+using VideoLibrary;
+
 namespace Downloader
 {
     public partial class Form1 : Form
@@ -19,6 +21,68 @@ namespace Downloader
         }
 
         private void btnDownload_Click(object sender, EventArgs e)
+        {
+            //UseSharpGrabber();
+            //UseVideoLibrary();
+            UseYoutubeExplode();
+        }
+
+        private void UseYoutubeExplode()
+        {
+            Task.Run(() =>
+            {
+
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void UseVideoLibrary()
+        {
+            Task.Run(() =>
+            {
+                var youTube = YouTube.Default;
+                var video = youTube.GetAllVideos(txtLink.Text);
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void UseSharpGrabber()
         {
             var grabber = GrabberBuilder.New()
                 .UseDefaultServices()
@@ -59,7 +123,7 @@ namespace Downloader
         private void GenerateOutputFile(string audioPath, string videoPath, GrabbedMedia videoStream)
         {
             FFmpeg.AutoGen.ffmpeg.RootPath = "C:\\Users\\erick\\source\\repos\\Downloader\\Test\\ffmpeg";
-            var outputPath = "D:\\Documents\\video";
+            var outputPath = "D:\\Documents\\" + videoStream.Title;
             if (string.IsNullOrWhiteSpace(outputPath))
                 throw new Exception("No output path is specified.");
             var merger = new MediaMerger(outputPath);
@@ -74,24 +138,26 @@ namespace Downloader
         [RequestFormLimits(MultipartBodyLengthLimit = 10L * 1024L * 1024L * 1024L)]
         private async Task<string> DownloadMedia(GrabbedMedia media, IGrabResult grabResult)
         {
+            var stopwatch = new Stopwatch();
+            var downloadRate = 1048576;
             lblProgress.Invoke(new Action(() => {
                 lblProgress.Text = "Esperando...";
                 prgDownload.Value = 0;
             }));
 
+            //"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" Testing video
             using var response = await Client.GetAsync(media.ResourceUri, HttpCompletionOption.ResponseHeadersRead);
             response.EnsureSuccessStatusCode();
             var totalBytes = response.Content.Headers.ContentLength ?? -1L;
             using var downloadStream = await response.Content.ReadAsStreamAsync();
-            using var streamReader = new StreamReader(downloadStream);
+            using var streamReader = new StreamReader(downloadStream, null,true,downloadRate, false);
 
-            var stopwatch = new Stopwatch();
-            var downloadRate = 1048576;
             var path = Path.GetTempFileName();
             TempFiles.Add(path);
             var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, downloadRate, true);
 
             var read = 0;
+            var loopReaded = 0;
             var totalRead = 0L;
             var buffer = new byte[downloadRate];
             CancellationToken token = new();
@@ -106,23 +172,39 @@ namespace Downloader
             do
             {
                 stopwatch.Start();
-                read = downloadStream.Read(buffer, 0, downloadRate);
+                read = await downloadStream.ReadAsync(buffer, 0, downloadRate);
                 var data = new byte[read];
                 buffer.ToList().CopyTo(0, data, 0, read);
 
-                await fileStream.WriteAsync(buffer.AsMemory(0, read));
+                await fileStream.WriteAsync(buffer, 0, read);
                 totalRead += read;
-                stopwatch.Stop();
+                loopReaded += read;
 
-                lblProgress.Invoke(new Action(() =>
+                if (stopwatch.ElapsedMilliseconds / 100 > 1)
                 {
-                    var downloadPercentage = totalRead * 1d / (totalBytes * 1d) * 100;
-                    lblProgress.Text = "Descargado: " + downloadPercentage.ToString("0.##") + "%";
-                    lblDownloaded.Text = (totalRead / 1000000.0).ToString("0.##") + " MB";
-                    lblSpeed.Text = (((read * 10000000.0) / stopwatch.ElapsedTicks) / 1024.0).ToString("0.##") + " KB/sec";
-                    prgDownload.Value = Convert.ToInt32(downloadPercentage);
-                }));
-                stopwatch.Reset();
+                    stopwatch.Stop();
+
+                    lblProgress.Invoke(new Action(() =>
+                    {
+                        var downloadPercentage = totalRead * 1d / (totalBytes * 1d) * 100;
+                        lblProgress.Text = "Descargado: " + downloadPercentage.ToString("0.##") + "%";
+                        lblDownloaded.Text = (totalRead / 1000000.0).ToString("0.##") + " MB";
+                        var speed = ((loopReaded * 1000.0) / stopwatch.ElapsedMilliseconds) / 1000.0;
+                        if(speed > 1000)
+                        {
+                            speed /= 1000;
+                            lblSpeed.Text = speed.ToString("0.##") + " MB/sec";
+                        }
+                        else
+                        {
+                            lblSpeed.Text = speed.ToString("0.##") + " KB/sec";
+                        }
+
+                        prgDownload.Value = Convert.ToInt32(downloadPercentage);
+                    }));
+                    stopwatch.Reset();
+                    loopReaded = 0;
+                }
             }
             while (read > 0);
             
